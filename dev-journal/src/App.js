0ba18b097da2e9542df90669ca150c1ef543d898
RyCab <rcabral0805@gmail.com>
2023-07-26 21:52:46 -0400

    latest

--- dev-journal/src/App.js
+++ dev-journal/src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
 import { Layout } from "./components/Layout/Layout";
 import React, { useEffect } from 'react';
-import { initGA, logPageView } from './analytics';
+import ReactGA from 'react-ga';
 
 function App() {
-  useEffect(() => {
-    initGA(); // Initialize Google Analytics
-    logPageView(); // Track the initial page view
-
-    // Additional tracking can be done for user interactions, events, etc.
-    // For example, you can track button clicks, form submissions, etc. using:
-    // ReactGA.event({ category: 'User Interaction', action: 'Clicked Button' });
-  }, []);
   return (
     <div className="App">
       <Layout/>
